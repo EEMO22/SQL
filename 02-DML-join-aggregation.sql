4a63bd0eec2eb3d364930ba1f4c8970aacfc6a16
강호진 <battlemech@naver.com>
2021-08-04 19:42:23 +0900

    JOIN, AGGREGATION 문제풀이

--- 02-DML-join-aggregation.sql
+++ 02-DML-join-aggregation.sql
@@ -198,7 +198,7 @@ SELECT department_id,
 FROM employees
 GROUP BY ROLLUP(department_id, job_id);
 
--- CUVE 함수
+-- CUBE 함수
 -- Cross Table 에 대한 Summary 를 함께 추출
 -- ROLLUP 함수에서 추출되는 Item Total 과 함께
 -- Column Total 값을 함께 추출
@@ -206,44 +206,6 @@ SELECT department_id, job_id, SUM(salary)
 FROM employees
 GROUP BY CUBE(department_id, job_id)
 ORDER BY department_id;
-
-
-
-
-
-
-
-
-
-
-------------------------------------------------------------------
--- 문제 1.
--- 직원들의 사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을
--- 조회하여 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순 으로 정렬하세요.
--- (106건)
-
-SELECT emp.employee_id 사번, 
-    emp.first_name || ' ' || emp.last_name 이름,
-    dept.department_name 부서이름
-FROM employees emp, departments dept
-WHERE dept.department_id = emp.department_id
-ORDER BY department_name ASC, employee_id DESC;
-
--- 문제 2.
--- employees 테이블의 job_id는 현재의 업무아이디를 가지고 있습니다.
--- 직원들의 사번(employee_id), 이름(firt_name), 급여(salary), 부서명(department_name), 
--- 현재업무(job_title)를 사번(employee_id) 오름차순 으로 정렬하세요.
--- 부서가 없는 Kimberely(사번 178)은 표시하지 않습니다.
--- (106건)
-
-SELECT emp.employee_id,
-    emp.first_name,
-    emp.salary,
-    dept.department_name,
-    jobs.job_title,
-FROM 
-
-
 
 
 
