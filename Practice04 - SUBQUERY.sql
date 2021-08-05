05869be2f780e8166b469d6426df389bd6e097d5
강호진 <battlemech@naver.com>
2021-08-05 15:37:17 +0900

    사용자 관리, 권한 관리

--- Practice04 - SUBQUERY.sql
+++ Practice04 - SUBQUERY.sql
@@ -11,6 +11,7 @@ WHERE salary < (SELECT AVG(salary) FROM employees);
 -- 이름(first_name), 급여(salary), 평균급여, 최대급여를 급여의 오름차순으로 정렬하여 출력하세요
 -- (51건)
 
-SELECT empleoyee_id 직원번호, first_name 이름, 
+SELECT employee_id 직원번호, first_name 이름, 
     salary 급여, ROUND(AVG(salary), 2) 평균급여, MAX(salary) 최대급여
 FROM employees 
+WHERE salary AND (SELECT salary FROM employees WHERE 
